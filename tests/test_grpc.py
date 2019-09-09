e454aa8dac09b6c14cd2fc7fccff88686329f80d
Estelle Maudet <e.maudet@qwant.com>
2019-09-09 15:39:49 +0200

    Improve on test_grpc.py
    Pass address and port as argument to the script.
    Print information on gRPC routes called.

--- tests/test_grpc.py
+++ tests/test_grpc.py
@@ -1,14 +1,24 @@
 # coding: utf-8
 
+import sys
+
 import grpc
 
 import grpc_nlu_pb2
 import grpc_nlu_pb2_grpc
 
-channel = grpc.insecure_channel('localhost:9010') 
+if len(sys.argv) != 2:
+    print('Usage: python test_grpc.py "address:port"')
+    exit(1)
+
+address = sys.argv[1]
+channel = grpc.insecure_channel(address) 
 stub = grpc_nlu_pb2_grpc.RouteNLUStub(channel)
 
 domains = stub.GetDomains(grpc_nlu_pb2.Empty())
+print("***")
+print("GetDomains:")
+print("***")
 print(domains)
 
 text_to_nlu = grpc_nlu_pb2.TextToParse(
@@ -29,8 +39,14 @@ def generate_to_nlu():
         yield elem    
 
 parsed = stub.GetNLU(text_to_nlu)
+print("***")
+print("GetNLU:")
+print("***")
 print(parsed)
 
 responses = stub.StreamNLU(generate_to_nlu())
+print("***")
+print("StreamNLU:")
+print("***")
 for response in responses:
     print(response)
